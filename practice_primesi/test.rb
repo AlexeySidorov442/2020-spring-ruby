40a8d853ea1aa454692d3e7d222980f2a09c408a
Aleksey <alesh.sidorov2017@yandex.ru>
2020-04-21 14:44:22 +0300

    delete test file

--- practice_primesi/test.rb
+++
@@ -1,9 +0,0 @@
-require 'optparse'
-
-options = {}
-OptionParser.new do |parser|
-  parser.on("-r", "--require LIBRARY",
-            "Require the LIBRARY before executing your script") do |lib|
-    puts "You required #{lib}!"
-  end
-end.parse!
